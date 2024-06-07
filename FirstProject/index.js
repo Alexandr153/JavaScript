f85da73ec3eb645084448a0ced912a83cba7708e
Alexandr153 <boss191105@bk.ru>
2024-06-07 17:28:27 +0600

    [+] Learning

--- FirstProject/index.js
+++ FirstProject/index.js
@@ -1,2 +1,13 @@
-document.write("<h2>Первая программа на JavaScript</h2>"); // выводим заголовок
-document.write("Привет мир!");                             // выводим обычный текст
+// document.write("Java Script says hello");
+// console.log("Hello world");
+// console.info("Hello world");
+// console.error("Ошибка номер 1");
+// console.warn("Предупреждение");
+
+var arr = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+console.log(arr)
+
+
