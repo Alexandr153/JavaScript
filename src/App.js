609904b4d7dede6b589c4ee51c7043d48475236d
Alexandr153 <boss191105@bk.ru>
2024-06-12 20:48:26 +0600

    Learn how to work with React + NodeJS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Button from './Components/Button/Button';
+
+function App() {
+  return <>
+  Im here!
+  <Button title={'Test'} disable={false} />
+  </>
+}
+
+export default App;
