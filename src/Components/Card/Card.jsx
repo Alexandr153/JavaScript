9e11ee830603109300d7061b2d7dea3c07f16e93
Alexandr153 <boss191105@bk.ru>
2024-06-26 23:36:36 +0600

    Working with Cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,22 +1,31 @@
 import React, {useState} from 'react';
 import './Card.css';
+import Button from '../Button/Button';
 
 function Card({upgrades}) {
 
     const [count, setCount] = useState(0);  
 
-    const {title, Image, price, id} = upgrades
+    const {title, Image, price, id} = upgrades;
+    
+    const handlerInecrement = ()=>{
+        setCount(count+1);
+    }
 
 
     return <div className='card'>
         <span className={`${count !==0 ? 'card__badge' : 'card__badge--hidden'}`}
-        >{count}</span>
+        ></span>
         <div className='.image__container'>
             <img src={Image} alt={title} />
         </div>
         <h4 className='card__title'>
-            {title} . <span className='class__price'>{price}</span>
+            {title}<span className='class__price'> {price}</span>
+            {count}
         </h4>
+        <div className='btn-container'>
+            <Button title={'Upgrade'} type={'add'} onClick={handlerInecrement}/>
+        </div>
     </div>;
 }
 export default Card;
--- src/db/db.js
+++ src/db/db.js
@@ -3,8 +3,8 @@ import storage from '../images/clock.png';
 
 export function getData(){
     return [
-        {title: "Reactor", price: 150, image: reactor, id:1},
-        {title: "Storage", price: 225, image: storage, id:2}
+        {title: "Reactor", price: 150 + ' tINCH', image: reactor, id:1},
+        {title: "Storage", price: 225 + ' tINCH', image: storage, id:2}
     ];
 }
 
